8d481f752bbac80ad6f4c3dfc75b9b8647123ab3
hyundai <dwayne.shim@gmail.com>
2018-06-29 15:33:18 +0900

    changed some for indexing

--- common/src/main/java/dwayne/shim/geogigani/common/indexing/TravelDataIndexField.java
+++ common/src/main/java/dwayne/shim/geogigani/common/indexing/TravelDataIndexField.java
@@ -1,30 +1,153 @@
 package dwayne.shim.geogigani.common.indexing;
 
+import org.apache.lucene.document.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TravelDataIndexField {
 
-    CONTENT_ID("contentid"),
-    CONTENT_TYPE_ID("contenttypeid"),
-    TITLE("title"),
-    TITLE_KEYWORDS("titlekeywords"),
-    ADDR1("addr1"),
-    ADDR2("addr2"),
-    ZIP_CODE("zipcode"),
-    TEL("tel"),
-    TEL_NAME("telname"),
-    OVERVIEW("overview"),
-    OVERVIEW_KEYWORDS("overviewkeywords"),
-    FIRST_IMAGE("firstimage"),
-    FIRST_IMAGE2("firstimage2"),
-    AREA_CODE("areacode"),
-    SIGUNGU_CODE("sigungucode"),
-    CREATED_TIME("createdtime"),
-    MODIFIED_TIME("modifiedtime"),
-    MAP_X("mapx"),
-    MAP_Y("mapy"),
-    MLEVEL("mlevel"),
-    CAT1("cat1"),
-    CAT2("cat2"),
-    CAT3("cat3");
+    CONTENT_ID("contentid") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    CONTENT_TYPE_ID("contenttypeid") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    TITLE("title") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    TITLE_KEYWORDS("titlekeywords") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    ADDR1("addr1") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    ADDR2("addr2") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    ZIP_CODE("zipcode") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    TEL("tel") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    TEL_NAME("telname") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    OVERVIEW("overview") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    OVERVIEW_KEYWORDS("overviewkeywords") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    FIRST_IMAGE("firstimage") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    FIRST_IMAGE2("firstimage2") {
+        @Override
+        public Field buildField(String value) {
+            return new TextField(label(), value, Field.Store.YES);
+        }
+    },
+    AREA_CODE("areacode") {
+        @Override
+        public Field buildField(String value) {
+            return new NumericDocValuesField(label(), Long.valueOf(value));
+        }
+    },
+    SIGUNGU_CODE("sigungucode") {
+        @Override
+        public Field buildField(String value) {
+            return new NumericDocValuesField(label(), Long.valueOf(value));
+        }
+    },
+    CREATED_TIME("createdtime") {
+        @Override
+        public Field buildField(String value) {
+            return new NumericDocValuesField(label(), Long.valueOf(value));
+        }
+    },
+    MODIFIED_TIME("modifiedtime") {
+        @Override
+        public Field buildField(String value) {
+            return new NumericDocValuesField(label(), Long.valueOf(value));
+        }
+    },
+    MAP_X("mapx") {
+        @Override
+        public Field buildField(String value) {
+            return new DoubleDocValuesField(label(), Double.valueOf(value));
+        }
+    },
+    MAP_Y("mapy") {
+        @Override
+        public Field buildField(String value) {
+            return new DoubleDocValuesField(label(), Double.valueOf(value));
+        }
+    },
+    MLEVEL("mlevel") {
+        @Override
+        public Field buildField(String value) {
+            return new NumericDocValuesField(label(), Long.valueOf(value));
+        }
+    },
+    CAT1("cat1") {
+        @Override
+        public Field buildField(String value) {
+            return new StringField(label(), value, Field.Store.YES);
+        }
+    },
+    CAT2("cat2") {
+        @Override
+        public Field buildField(String value) {
+            return new StringField(label(), value, Field.Store.YES);
+        }
+    },
+    CAT3("cat3") {
+        @Override
+        public Field buildField(String value) {
+            return new StringField(label(), value, Field.Store.YES);
+        }
+    };
+
+    public abstract Field buildField(String value);
 
     private String label;
     private TravelDataIndexField(String _label) {
@@ -33,5 +156,12 @@ public enum TravelDataIndexField {
 
     public String label() {
         return label;
+    }
+
+    public static Map<String, TravelDataIndexField> map() {
+        Map<String, TravelDataIndexField> map = new HashMap<>();
+        for(TravelDataIndexField field : values())
+            map.put(field.label, field);
+        return Collections.unmodifiableMap(map);
     }
 }
