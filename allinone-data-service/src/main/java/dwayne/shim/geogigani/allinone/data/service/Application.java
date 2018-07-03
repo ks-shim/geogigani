6bdb3515e7c93b6f1679047c6938144067b6897c
hyundai <dwayne.shim@gmail.com>
2018-07-03 13:37:14 +0900

    developing allinone-data-service ...

--- allinone-data-service/src/main/java/dwayne/shim/geogigani/allinone/data/service/Application.java
+++ allinone-data-service/src/main/java/dwayne/shim/geogigani/allinone/data/service/Application.java
@@ -11,7 +11,9 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 @Log4j2
 @SpringBootApplication
@@ -54,17 +56,21 @@ public class Application extends SpringBootServletInitializer {
         IdWeightStorage storage = new IdWeightStorage();
 
         // 1. read from original files ...
-        log.info("Start reading from the original location files ...");
+        log.info("Start reading the original location files ...");
         File dir = new File(locationOriginalDir);
         for(File file : dir.listFiles())
             storage.addIdWeight(file.getName());
-        log.info("Finished reading from the original location files ...");
+        log.info("Finished reading the original location files ...");
 
         // 2. read from snapshot (count, weight and score)
+        log.info("Start reading all location snapshots ...");
         storage.readAllSnapshots(locationSnapshotDir);
+        log.info("Finished reading all location snapshots ...");
 
         // 3. get ready to service (sort and pick topN)
+        log.info("Start sorting and picking top-n location ...");
         storage.sortAndPickTopN(locationTopN);
+        log.info("Finished sorting and picking top-n location ...");
 
         return storage;
     }
@@ -74,10 +80,27 @@ public class Application extends SpringBootServletInitializer {
         IdWeightStorage storage = new IdWeightStorage();
 
         // 1. read from keyword list file ...
+        log.info("Start reading the keyword list ...");
+        try (BufferedReader in = new BufferedReader(new FileReader(keywordListFile))){
+            String keyword = null;
+            while((keyword = in.readLine()) != null) {
+                keyword = keyword.trim();
+                if(keyword.isEmpty()) continue;
+
+                storage.addIdWeight(keyword);
+            }
+        }
+        log.info("Finished reading the keyword list ...");
 
         // 2. read from snapshot (count, weight and score)
+        log.info("Start reading all keyword snapshots ...");
+        storage.readAllSnapshots(keywordSnapshotDir);
+        log.info("Finished reading all keyword snapshots ...");
 
         // 3. get ready to service (sort and pick topN)
+        log.info("Start sorting and picking top-n keywords ...");
+        storage.sortAndPickTopN(keywordTopN);
+        log.info("Finished sorting and picking top-n keywords ...");
 
         return storage;
     }
