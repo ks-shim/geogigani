26a22e630583658372b48a5bec98ed800bf8b3aa
hyundai <dwayne.shim@gmail.com>
2018-07-04 15:30:27 +0900

    developing front-service ...

--- front-service/src/main/java/dwayne/shim/geogigani/front/service/controller/FrontController.java
+++ front-service/src/main/java/dwayne/shim/geogigani/front/service/controller/FrontController.java
@@ -1,7 +1,16 @@
 package dwayne.shim.geogigani.front.service.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/main-page")
 public class FrontController {
+
+    @RequestMapping(value = {"/popular-locations"}, produces = "application/json; charset=utf8", method = {RequestMethod.GET})
+    public String showPopularLocations(Model model) {
+        return "main-page";
+    }
 }
