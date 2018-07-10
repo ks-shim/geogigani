5da01c1e8fbde3d1574feceaa7bed0c70bda9e52
hyundai <dwayne.shim@gmail.com>
2018-07-11 06:52:05 +0900

    added LocationDistance Class

--- crawler/src/main/java/dwayne/shim/geogigani/crawler/TravelDataCrawler.java
+++ crawler/src/main/java/dwayne/shim/geogigani/crawler/TravelDataCrawler.java
@@ -314,7 +314,7 @@ public class TravelDataCrawler {
 
         // 3. processing ..
         TravelDataCrawler tdc = new TravelDataCrawler();
-        int startPage = 19;
+        int startPage = 20;
         for(int i=0; i<authKeys.length; i++)
             tdc.execute(authKeys[i], numOfRows, appName, osName, new File(outDirectory), startPage+i, startPage+i);
     }
