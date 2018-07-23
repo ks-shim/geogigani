b7c122d94bcd899e82e20e3669d42e928a07ec96
hyundai <dwayne.shim@gmail.com>
2018-07-24 08:11:45 +0900

    changing ui

--- front-service/src/main/java/dwayne/shim/geogigani/front/service/Application.java
+++ front-service/src/main/java/dwayne/shim/geogigani/front/service/Application.java
@@ -24,6 +24,6 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args); //test
+        SpringApplication.run(Application.class, args);
     }
 }
